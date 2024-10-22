ff4296bc2a24ae66ce00924352c15968e04d098a
Jhone-cmd <jhone.1941@gmail.com>
2024-10-22 09:26:26 -0300

    chore: create env.example and update seed.ts

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -10,7 +10,11 @@ async function seed() {
     .insert(goals)
     .values([
       { title: "estudar", desiredWeeklyFrequency: 5 },
-      { title: "treinar", desiredWeeklyFrequency: 4 },
+      { title: "programação", desiredWeeklyFrequency: 3 },
+      { title: "treinar muay thai / capoeira", desiredWeeklyFrequency: 3 },
+      { title: "fazer exercícios", desiredWeeklyFrequency: 2 },
+      { title: "meditar", desiredWeeklyFrequency: 2 },
+      { title: "não mexer no celular", desiredWeeklyFrequency: 1 },
     ])
     .returning();
 
@@ -18,7 +22,11 @@ async function seed() {
 
   await db.insert(goalCompletions).values([
     { goalId: result[0].id, createdAt: startOfWeek.toDate() },
-    { goalId: result[1].id, createdAt: startOfWeek.add(1, "day").toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[2].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[3].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[4].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[5].id, createdAt: startOfWeek.add(1, "day").toDate() },
   ]);
 }
 
